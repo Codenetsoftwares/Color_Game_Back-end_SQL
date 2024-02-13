18d29eba57fdbb914bb630418539120420ef9d04
riktadeyy <rikta.dey.cns@gmail.com>
2024-02-13 15:42:43 +0530

    feat

--- models/user.model.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-export const User = new mongoose.model("user", new mongoose.Schema({
-    firstName:{type:String,required:true},
-    lastName:{type:String,required:true},
-    userName:  { type: String, required: true },
-    phoneNumber:{type:Number,required:true},
-    password:   { type: String, required: true },
-    roles: [{ type: String, required: true, default: 'User'}],
-}), 'user');
