924c08d2f393b3f00ea2057462b2fdc8e0b13337
riktadeyy <rikta.dey.cns@gmail.com>
2024-03-18 12:54:43 +0530

    complete sql

--- server.js
+++ server.js
@@ -1,56 +1,52 @@
 // server.js
-import mysql from "mysql2";
-import express from "express";
-import bodyParser from "body-parser";
-import dotenv from "dotenv";
-import { AdminRoute } from "./routes/admin.route.js";
-import { errorHandler } from "./middleware/ErrorHandling.js";
-
+import mysql from 'mysql2';
+import express from 'express';
+import bodyParser from 'body-parser';
+import dotenv from 'dotenv';
+import { AdminRoute } from './routes/admin.route.js';
+import { errorHandler } from './middleware/ErrorHandling.js';
 
 import cors from 'cors';
-import { UserRoute } from "./routes/user.route.js";
+import { UserRoute } from './routes/user.route.js';
 
 dotenv.config();
 const app = express();
 
 app.use(bodyParser.json());
 
-
-
 app.use(bodyParser.json({ limit: '50mb' }));
 app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 app.use(express.urlencoded({ extended: true }));
-const allowedOrigins = process.env.FRONTEND_URI.split(",");
+const allowedOrigins = process.env.FRONTEND_URI.split(',');
 app.use(cors({ origin: allowedOrigins }));
 
-
 const pool = mysql.createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
-    waitForConnections: true,
-    connectionLimit: 10,
-    queueLimit: 0,
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
 });
 
 pool.getConnection((err, connection) => {
-    if (err) {
-        console.log("Error to connecting Database: " + err.message);
-    } else {
-        console.log("Database connection successfully");
-        connection.release(); 
-    }
+  if (err) {
+    console.log('Error to connecting Database: ' + err.message);
+  } else {
+    console.log('Database connection successfully');
+    connection.release();
+  }
 });
 
 app.get('/', (req, res) => {
-    res.send('Hello World!');
-  });
-  
+  res.send('Hello World!');
+});
+
 AdminRoute(app);
-UserRoute(app)
+UserRoute(app);
 app.use(errorHandler);
 
 app.listen(8080, () => {
-    console.log("App is running on - http://localhost:8080");
+  console.log('App is running on - http://localhost:8080');
 });
