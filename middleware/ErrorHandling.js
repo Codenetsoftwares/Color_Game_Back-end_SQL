ebeecfd6080ebac53748d3acf7ba002b13ea9a8c
masudcns <masudur07cns@gmail.com>
2024-05-03 19:25:56 +0530

    convert sql Slider Apis

---
+++ middleware/ErrorHandling.js
@@ -0,0 +1,16 @@
+export const notFound = (req, res, next) => {
+  const err = new Error(`can't find ${req.originalUrl} on server!`);
+  err.statusCode = 404;
+  err.message = 'Failed ';
+  next(err);
+};
+
+export const errorHandler = (err, req, res, next) => {
+  const statusCode = err.status || 500;
+  const message = err.message || 'Internal Server Error';
+  res.status(statusCode).json({
+    success: false,
+    message: message,
+    status: statusCode,
+  });
+};
