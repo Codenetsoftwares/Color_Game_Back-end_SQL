2445c0402ee30942ba8f5fdf09ef9797ac652975
riktadeyy <rikta.dey.cns@gmail.com>
2024-08-29 13:29:32 +0530

    send auth in endpoint

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -1,4 +1,6 @@
+import { string } from '../constructor/string.js';
 import { adminLogin, loginUser, resetPassword, trashUser, restoreTrashUser, logout } from '../controller/auth.controller.js';
+import { authorize } from '../middleware/auth.js';
 import customErrorHandler from '../middleware/customErrorHandler.js';
 import { loginSchema, logOutValidate, resetPasswordSchema, trashUserSchema } from '../schema/commonSchema.js';
 
@@ -6,14 +8,14 @@ export const authRoute = (app) => {
   // done
   app.post('/api/admin-login', loginSchema, customErrorHandler, adminLogin);
   // done
-  app.post('/api/user-login',loginSchema, customErrorHandler, loginUser);
+  app.post('/api/user-login', loginSchema, customErrorHandler, loginUser);
   // done
-  app.post('/api/reset-password', resetPasswordSchema, customErrorHandler, resetPassword);
+  app.post('/api/reset-password', resetPasswordSchema, customErrorHandler, authorize([string.User]), resetPassword);
   // done
   app.post('/api/extrernal/trash-user', trashUserSchema, customErrorHandler, trashUser);
   // done
   app.post('/api/extrernal/restore-trash-user', trashUserSchema, customErrorHandler, restoreTrashUser);
 
-  app.post('/api/user-logout',logOutValidate, customErrorHandler, logout);
+  app.post('/api/user-logout', logOutValidate, customErrorHandler, logout);
 
 };
