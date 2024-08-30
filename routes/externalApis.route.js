e30329b8b2f6dc0cbc58aa1a6a1239afd2243e42
riktadeyy <rikta.dey.cns@gmail.com>
2024-08-30 18:06:42 +0530

    authenticate external apis for bet history and p/l

--- routes/externalApis.route.js
+++ routes/externalApis.route.js
@@ -1,13 +1,14 @@
 import { calculateExternalProfitLoss, getExternalUserBetHistory, marketExternalProfitLoss, runnerExternalProfitLoss } from "../controller/externalApis.controller.js";
 import customErrorHandler from "../middleware/customErrorHandler.js";
+import { authenticateSuperAdmin } from "../middleware/whiteLabelAuth.js";
 import { betHistorySchema, calculateProfitLossSchema, marketProfitLossSchema, runnerProfitLossSchema } from "../schema/commonSchema.js";
 
 export const externalApisRoute = (app) => {
-    app.get('/api/external-user-betHistory/:userName/:gameId', betHistorySchema, customErrorHandler, getExternalUserBetHistory);
+    app.get('/api/external-user-betHistory/:userName/:gameId', betHistorySchema, customErrorHandler,authenticateSuperAdmin, getExternalUserBetHistory);
 
-    app.get('/api/external-profit_loss/:userName', calculateProfitLossSchema, customErrorHandler, calculateExternalProfitLoss);
+    app.get('/api/external-profit_loss/:userName', calculateProfitLossSchema, customErrorHandler,authenticateSuperAdmin, calculateExternalProfitLoss);
 
-    app.get('/api/external-profit_loss_market/:userName/:gameId', marketProfitLossSchema, customErrorHandler, marketExternalProfitLoss);
+    app.get('/api/external-profit_loss_market/:userName/:gameId', marketProfitLossSchema, customErrorHandler,authenticateSuperAdmin, marketExternalProfitLoss);
 
-    app.get('/api/external-profit_loss_runner/:userName/:marketId', runnerProfitLossSchema, customErrorHandler, runnerExternalProfitLoss);
+    app.get('/api/external-profit_loss_runner/:userName/:marketId', runnerProfitLossSchema, customErrorHandler,authenticateSuperAdmin, runnerExternalProfitLoss);
 }
