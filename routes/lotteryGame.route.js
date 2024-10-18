d1af41ed76f8748bd0d37e68546ee4d9573cab81
riktadeyy <rikta.dey.cns@gmail.com>
2024-10-18 15:46:11 +0530

    lottery implementation

--- routes/lotteryGame.route.js
+++ routes/lotteryGame.route.js
@@ -1,33 +1,12 @@
 import { string } from "../constructor/string.js";
-import {
-  dummyData,
-  getLotteryGame,
-  getUser,
-  getUserPurchases,
-  lotteryAmount,
-  purchaseLotteryTicket,
-} from "../controller/lotteryGame.controller.js";
+import { purchaseLottery, searchTicket } from "../controller/lotteryGame.controller.js";
 import { authorize } from "../middleware/auth.js";
-import customErrorHandler from "../middleware/customErrorHandler.js";
-import { validatePurchaseLotteryTicket } from "../schema/commonSchema.js";
 
 export const lotteryRoute = (app) => {
-  app.get("/api/get-lottery-game", getLotteryGame);
 
-  app.get("/api/get-users", getUser); //Not use
+  app.post("/api/search-ticket", authorize([string.User]), searchTicket);
 
-  app.post(
-    "/api/purchase-lottery",
-    validatePurchaseLotteryTicket,
-    customErrorHandler,
-    authorize([string.User]),
-    purchaseLotteryTicket
-  );
+  app.post("/api/purchase-lottery", authorize([string.User]), purchaseLottery);
 
-  app.get("/api/user-lotteryAmount/:lotteryId", authorize([string.User]), lotteryAmount);
 
-  app.get("/api/user-purchases", authorize([string.User]), getUserPurchases);
-
-  // dummy- data Api
-  app.get("/api/lotteries-dummyData", dummyData);
 };
