2007282348a1d00903a51f4003aeecdc9517c612
riktadeyy <rikta.dey.cns@gmail.com>
2024-11-25 16:57:07 +0530

    modify

--- routes/lotteryGame.route.js
+++ routes/lotteryGame.route.js
@@ -17,36 +17,36 @@ import {
 } from "../controller/lotteryGame.controller.js";
 import { authorize } from "../middleware/auth.js";
 import customErrorHandler from "../middleware/customErrorHandler.js";
-import {  validateCreateLotteryP_L, validateDateWiseMarkets, validateGetLotteryBetHistory, validateMarketId, validatePurchaseHistory, validatePurchaseLottery, validateRemoveExposer, validateSearchTickets, validateUpdateBalance } from "../schema/commonSchema.js";
+import { validateCreateLotteryP_L, validateDateWiseMarkets, validateGetLotteryBetHistory, validateMarketId, validatePurchaseHistory, validatePurchaseLottery, validateRemoveExposer, validateSearchTickets, validateUpdateBalance } from "../schema/commonSchema.js";
 
 export const lotteryRoute = (app) => {
 
-  app.post("/api/search-ticket", validateSearchTickets, customErrorHandler, authorize([string.User]), searchTicket); 
+  app.post("/api/search-ticket", validateSearchTickets, customErrorHandler, authorize([string.User]), searchTicket);
 
-  app.post("/api/purchase-lottery/:marketId",validatePurchaseLottery,customErrorHandler, authorize([string.User]), purchaseLottery); 
+  app.post("/api/purchase-lottery/:marketId", validatePurchaseLottery, customErrorHandler, authorize([string.User]), purchaseLottery);
 
-  app.post('/api/purchase-history/:marketId',validatePurchaseHistory,customErrorHandler, authorize([string.User]), purchaseHistory); 
+  app.post('/api/purchase-history/:marketId', validatePurchaseHistory, customErrorHandler, authorize([string.User]), purchaseHistory);
 
-  app.get('/api/get-range', getTicketRange); 
+  app.get('/api/get-range', getTicketRange);
 
   app.get("/api/prize-results", authorize([string.User]), getResult);
 
-  app.get('/api/user-getAllMarket', authorize([string.User]), getMarkets)  
+  app.get('/api/user-getAllMarket', authorize([string.User]), getMarkets)
 
-  app.post("/api/users/update-balance",validateUpdateBalance,customErrorHandler, updateBalance)
+  app.post("/api/users/update-balance", validateUpdateBalance, customErrorHandler, updateBalance)
 
-  app.post("/api/users/remove-exposer",validateRemoveExposer,customErrorHandler, removeExposer)
+  app.post("/api/users/remove-exposer", validateRemoveExposer, customErrorHandler, removeExposer)
 
-  app.get('/api/user-lottery-results/:marketId',validateMarketId,customErrorHandler, getLotteryResults);
+  app.get('/api/user-lottery-results/:marketId', validateMarketId, customErrorHandler, getLotteryResults);
 
-  app.post('/api/lottery-profit-loss',validateCreateLotteryP_L,customErrorHandler, createLotteryP_L)
+  app.post('/api/lottery-profit-loss', validateCreateLotteryP_L, customErrorHandler, createLotteryP_L)
 
   app.get('/api/lottery-profit-loss', authorize([string.User]), getLotteryP_L);
 
-  app.post('/api/lottery-bet-history',validateGetLotteryBetHistory,customErrorHandler, authorize([string.User]), getLotteryBetHistory);
+  app.post('/api/lottery-bet-history', validateGetLotteryBetHistory, customErrorHandler, authorize([string.User]), getLotteryBetHistory);
 
-  app.get("/api/user/markets-dateWise", validateDateWiseMarkets,customErrorHandler,authorize([string.User]), dateWiseMarkets);
+  app.get("/api/user/markets-dateWise", validateDateWiseMarkets, customErrorHandler, authorize([string.User]), dateWiseMarkets);
 
-  app.get('/api/user/getMarkets', authorize([string.User]),getAllMarket )
+  app.get('/api/user/getMarkets', authorize([string.User]), getAllMarket)
 
 };
