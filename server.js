eab61efd1ce94b88229bc9e1a656b6db2758b26b
riktadeyy <rikta.dey.cns@gmail.com>
2024-12-23 11:53:25 +0530

    make synch alter false

--- server.js
+++ server.js
@@ -146,7 +146,7 @@ app.get('/events', (req, res) => {
 });
 
 sequelize
-  .sync({ alter: true })
+  .sync({ alter: false })
   .then(() => {
     console.log('Database & tables created!');
     app.listen(process.env.PORT, () => {
